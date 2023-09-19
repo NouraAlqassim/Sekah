4d394a2f7affdcf111ad2a055340243079585be5
Patrick Saoud <patricksaoud@gmail.com>
2023-09-19 06:57:31 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import numpy as np
+
+
+
+from super_gradients.training import Trainer
+from super_gradients.training import dataloaders
+from super_gradients.training.dataloaders.dataloaders import (
+    coco_detection_yolo_format_train,
+    coco_detection_yolo_format_val
+)
+from super_gradients.training import models
+from super_gradients.training.losses import PPYoloELoss
+from super_gradients.training.metrics import (
+    DetectionMetrics_050,
+    DetectionMetrics_050_095
+)
+from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
+
